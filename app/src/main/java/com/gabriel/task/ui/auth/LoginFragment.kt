0372f16fda34d8d611433d63e66f3110f40fe9b3
20232tiimi0217 <gpoloni4@icloud.com>
2026-03-20 15:47:09 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/gabriel/task/ui/auth/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.gabriel.task.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gabriel.task.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/gabriel/task/ui/auth/RegisterFragment.kt
@@ -0,0 +1,18 @@
+package com.gabriel.task.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gabriel.task.R
+
+class RegisterFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
