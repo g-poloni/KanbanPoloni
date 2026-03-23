99b73cdbfbb6d49afca64b27ef98954fb5665b46
20232tiimi0217 <gpoloni4@icloud.com>
2026-03-23 18:07:16 -0300

    FIGURA 31

--- app/src/main/java/com/gabriel/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/gabriel/task/ui/auth/LoginFragment.kt
@@ -6,14 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gabriel.task.R
+import com.gabriel.task.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
